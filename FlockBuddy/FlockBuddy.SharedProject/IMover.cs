b9f43d459ce3f3f07322ab2641622abc772f3ffa
Dan Manning <dmanning23@hotmail.com>
2017-01-23 22:25:16 -0600

    moved into nuget packages

---
+++ FlockBuddy/FlockBuddy.SharedProject/IMover.cs
@@ -0,0 +1,19 @@
+using Microsoft.Xna.Framework;
+using PrimitiveBuddy;
+
+namespace FlockBuddy
+{
+	/// <summary>
+	/// An interface defining an entity that moves.
+	/// </summary>
+	public interface IMover : IBaseEntity
+	{
+		Vector2 Heading { get; }
+
+		float Speed { get; }
+
+		Vector2 Velocity { get; }
+
+		void Render(IPrimitive prim, Color color);
+	}
+}
